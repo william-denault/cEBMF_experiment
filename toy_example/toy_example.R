d02ada2c2706600a6b0498f1721a24c6d64e4215
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-04-18 12:55:10 -0500

    Added nmf fit to toy_example.R.

--- toy_example/toy_example.R
+++ toy_example/toy_example.R
@@ -1,3 +1,4 @@
+library(NNLM)
 library(ggplot2)
 library(cowplot)
 set.seed(1)
@@ -25,3 +26,13 @@ print(table(true = sim$cluster,est = res$cluster))
 #    1 313   5   1
 #    2   1   0 335
 #    3   2 341   2
+set.seed(1)
+fit <- nnmf(scale(Z,center = TRUE,scale = FALSE),
+            k = 3,method = "scd",loss = "mse",verbose = 2,
+            n.threads = 2,rel.tol = 1e-8,max.iter = 100)
+res2 <- kmeans(fit$W,centers = 3)
+print(table(true = sim$cluster,est = res2$cluster))
+print(round(rbind(colMeans(fit$W[sim$cluster == 1,]),
+                  colMeans(fit$W[sim$cluster == 2,]),
+                  colMeans(fit$W[sim$cluster == 3,])),
+            digits = 2))
