ee590c8f839fe64d09adf5654807da2bbce906b1
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-04-18 10:47:18 -0500

    Added script toy_example.R to analyze toy data set.

---
+++ toy_example/toy_example.R
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(cowplot)
+set.seed(1)
+cluster_colors <- c("darkorange","dodgerblue","darkblue")
+load("fit_plot_Neurips.RData")
+Z <- file_pc$Z
+sim <- with(file_pc,data.frame(x = x,y = y,cluster = 0))
+for (k in 1:3)
+  sim[file_pc$L[,k] == 1,"cluster"] <- k
+sim <- transform(sim,cluster = factor(cluster))
+p1 <- ggplot(sim,aes(x = x,y = y,color = cluster)) +
+  geom_point() +
+  scale_color_manual(values = cluster_colors) +
+  theme_cowplot(font_size = 12)
+pca <- prcomp(Z)
+pdat2 <- cbind(sim,pca$x[,1:2])
+p2 <- ggplot(pdat2,aes(x = PC1,y = PC2,color = cluster)) +
+  geom_point() +
+  scale_color_manual(values = cluster_colors) +
+  theme_cowplot(font_size = 12)
+res <- kmeans(pca$x[,1:2],centers = 3)
+print(table(true = sim$cluster,est = res$cluster))
+#     est
+# true   1   2   3
+#    1 313   5   1
+#    2   1   0 335
+#    3   2 341   2
